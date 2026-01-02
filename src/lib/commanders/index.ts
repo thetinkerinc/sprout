d13e7c3852d2fa4ab18e8ef9474d1f0cb7323f78
Shane Brass <shanebrass1618@gmail.com>
2026-01-01 21:36:50 -0700

    Declaring commander types

--- src/lib/commanders/index.ts
+++ src/lib/commanders/index.ts
@@ -3,21 +3,26 @@ import { makeCommander } from '@thetinkerinc/commander';
 
 import { getDb } from '../db';
 
-export const Anonymous = makeCommander(async () => {
+type Commander<T> = ReturnType<typeof makeCommander<Promise<T>>>;
+type Db = Awaited<ReturnType<typeof getDb>>;
+
+export const Anonymous: Commander<{ db: Db }> = makeCommander(async () => {
 	const db = await getDb();
 	return {
 		db
 	};
 });
 
-export const Authenticated = makeCommander(async ({ event }) => {
-	const { userId } = event.locals.auth();
-	if (!userId) {
-		error(403, "You don't have permission to perform this action");
+export const Authenticated: Commander<{ userId: string; db: Db }> = makeCommander(
+	async ({ event }) => {
+		const { userId } = event.locals.auth();
+		if (!userId) {
+			error(403, "You don't have permission to perform this action");
+		}
+		const db = await getDb();
+		return {
+			userId,
+			db
+		};
 	}
-	const db = await getDb();
-	return {
-		userId,
-		db
-	};
-});
+);
