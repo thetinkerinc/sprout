4b30c870300e83a2d6d039823d577b596e15a51e
Shane Brass <shanebrass1618@gmail.com>
2026-01-03 17:09:53 -0700

    Adding vite plugin

---
+++ src/lib/vite/index.ts
@@ -0,0 +1,32 @@
+import { execSync } from 'node:child_process';
+
+import type { Plugin } from 'vite';
+
+export function sprout(): Plugin {
+	let registered = false;
+
+	function shutdown(exit: boolean) {
+		return () => {
+			try {
+				execSync('docker compose down', { stdio: 'ignore' });
+			} catch (_err) {} // eslint-disable-line no-empty
+			if (exit) {
+				process.exit();
+			}
+		};
+	}
+
+	return {
+		name: 'stop-db-on-exit',
+		apply: 'serve',
+		configureServer() {
+			if (registered) {
+				return;
+			}
+			registered = true;
+			process.on('SIGINT', shutdown(true));
+			process.on('SIGTERM', shutdown(true));
+			process.on('exit', shutdown(false));
+		}
+	};
+}
