ba7eef60de3fde1f53f4556c56eb299a1a618a5c
Shane Brass <shanebrass1618@gmail.com>
2026-01-04 11:29:35 -0700

    Adding navigation utils

---
+++ src/lib/navigation/index.ts
@@ -0,0 +1,24 @@
+import { page } from '$app/state';
+import { goto } from '$app/navigation';
+import { resolve } from '$app/paths';
+
+import type { Pathname } from '$app/types';
+
+export async function updateSearch(params: { [key: string]: string | undefined }) {
+	const currentParams = page.url.searchParams;
+	Object.entries(params).forEach(([k, v]) => {
+		if (v) {
+			currentParams.set(k, v);
+		} else {
+			currentParams.delete(k);
+		}
+	});
+	let query = currentParams.toString();
+	if (query) {
+		query = '?' + query;
+	}
+	await goto(resolve((page.url.pathname + query) as Pathname), {
+		replaceState: true,
+		invalidateAll: true
+	});
+}
